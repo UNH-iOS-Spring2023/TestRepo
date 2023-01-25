747f404eed1ca3165f17123b9ace036b464b70d5
fpellicano <fpellicano@newhaven.edu>
2023-01-24 20:12:03 -0500

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by forensics on 1/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/testappApp.swift
@@ -0,0 +1,17 @@
+//
+//  testappApp.swift
+//  Shared
+//
+//  Created by forensics on 1/24/23.
+//
+
+import SwiftUI
+
+@main
+struct testappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
