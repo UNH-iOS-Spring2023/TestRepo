a48df49194ea61b2573949bb3185ef7ffca54753
fpellicano <fpellicano@newhaven.edu>
2023-01-24 20:18:38 -0500

    Changed display text
    Changed the text from hello world to class. This makes it more clear that I am only addressing the students of my class, not everyone in the world.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -11,7 +11,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Hello, class!")
             .padding() // Test text showing hello
     }
 }
